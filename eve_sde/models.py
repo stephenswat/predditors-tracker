46f985c45ba3f6deeb6b71bbcfc8fa11de3c3bdd
Stephen Swatman <stephenswat@gmail.com>
2017-01-22 21:35:12 +0100

    Add __str__ methods to the SDE app.

--- eve_sde/models.py
+++ eve_sde/models.py
@@ -5,10 +5,16 @@ class Region(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(db_index=True, unique=True, max_length=64)
 
+    def __str__(self):
+        return self.name
+
 class Constellation(models.Model):
     id = models.IntegerField(primary_key=True)
     region = models.ForeignKey(Region, related_name='constellations', db_index=True)
     name = models.CharField(db_index=True, unique=True, max_length=64)
+
+    def __str__(self):
+        return self.name
 
 class SolarSystem(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -16,3 +22,6 @@ class SolarSystem(models.Model):
     name = models.CharField(db_index=True, unique=True, max_length=64)
 
     security = models.FloatField()
+
+    def __str__(self):
+        return self.name
