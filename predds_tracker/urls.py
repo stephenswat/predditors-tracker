d05f791881e03d33e6197d3aad9322ec0746b2b3
awbdallas <Austin.W.Briggs@gmail.com>
2017-01-22 21:58:06 -0700

    Adding Help Page
    Just small change to add a help page.

--- predds_tracker/urls.py
+++ predds_tracker/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^map/(?P<region>[0-9]+)/$', views.map, name='display_region'),
     url(r'^map/(?P<region>[0-9]+)/svg/', views.map_svg, name='region_svg'),
+    url(r'^help/', views.help, name='help'),
     url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^', include('social.apps.django_app.urls', namespace='social')),
     url(r'^$', views.home),
