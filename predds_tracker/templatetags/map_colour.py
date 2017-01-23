094106494251228f306aaf62468270dbfcb6908a
Stephen Swatman <stephenswat@gmail.com>
2017-01-23 22:56:25 +0100

    Improve readability of low-activity systems.

--- predds_tracker/templatetags/map_colour.py
+++ predds_tracker/templatetags/map_colour.py
@@ -11,7 +11,7 @@ def npc_kill_colour(value):
 
     if kills <= 1000:
         gradient = list(Color('white').range_to(Color('#BFFF00'), 10))
-        return gradient[kills // 100]
+        return gradient[(kills + 99) // 100]
     else:
         gradient = list(Color('#BFFF00').range_to(Color('red'), 30))
-        return gradient[min(kills - 1000, 2000) // 100]
+        return gradient[min(kills + 99 - 1000, 2000) // 100]
