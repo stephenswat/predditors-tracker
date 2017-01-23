25f6bff784491d41684c7fb6f542b5bff962eebe
Stephen Swatman <stephenswat@gmail.com>
2017-01-24 00:07:14 +0100

    Nest region URLs.

--- predds_tracker/urls.py
+++ predds_tracker/urls.py
@@ -3,12 +3,16 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from predds_tracker import views
 
+regionpatterns = [
+    url(r'^$', views.map, name='display_region'),
+    url(r'^svg/$', views.map_svg, name='region_svg'),
+]
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^log/$', views.log, name='log'),
     url(r'^profile/$', views.profile, name='profile'),
-    url(r'^map/(?P<region>[0-9]+)/$', views.map, name='display_region'),
-    url(r'^map/(?P<region>[0-9]+)/svg/', views.map_svg, name='region_svg'),
+    url(r'^region/(?P<region>[0-9]+)/', include(regionpatterns)),
     url(r'^help/', views.help, name='help'),
     url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^', include('social.apps.django_app.urls', namespace='social')),
