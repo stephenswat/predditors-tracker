6e7392f0b2e83247099433d1af5628dd9d204038
Stephen Swatman <stephenswat@gmail.com>
2017-01-29 00:56:28 +0100

    Add admins for most of the models.

--- predds_tracker/admin.py
+++ predds_tracker/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from predds_tracker.models import Character, LocationRecord
+from predds_tracker.models import Character, LocationRecord, Alt, SystemMetadata
 
 class LocationRecordAdmin(admin.ModelAdmin):
     date_hierarchy = 'time'
     list_display = ('character', 'system', 'online', 'station_id', 'ship_id', 'ship_type_id', 'time')
 
+class AltAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'main', 'track')
+
+class SystemMetadataAdmin(admin.ModelAdmin):
+    list_display = ('system', 'important')
+
 admin.site.register(Character, UserAdmin)
 admin.site.register(LocationRecord, LocationRecordAdmin)
+admin.site.register(Alt, AltAdmin)
+admin.site.register(SystemMetadata, SystemMetadataAdmin)
