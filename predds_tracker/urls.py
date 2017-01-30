d2f671cb7ff41304deb608037612fc6b0cc8b3b3
JazzyJosh <JazzyJosh@users.noreply.github.com>
2017-01-30 02:43:10 +0000

    Change map region argument to region_id, since that's what it is

--- predds_tracker/urls.py
+++ predds_tracker/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'^alts/$', views.all_alts, name='alts'),
     url(r'^profile/delete/$', views.delete_account, name='delete_account'),
     url(r'^profile/$', views.profile, name='profile'),
-    url(r'^region/(?P<region>[0-9]+)/', include(regionpatterns)),
+    url(r'^region/(?P<region_id>[0-9]+)/', include(regionpatterns)),
     url(r'^help/', views.help, name='help'),
     url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^login/warning/', views.login_warning, name='login_warning'),
