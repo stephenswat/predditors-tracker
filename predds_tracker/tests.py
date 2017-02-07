50636e247b2d9570bdf498a98592b041e517c7b7
Stephen Swatman <stephenswat@gmail.com>
2017-02-07 23:28:56 +0100

    More tests! Yay!

--- predds_tracker/tests.py
+++ predds_tracker/tests.py
@@ -1,6 +1,7 @@
 import unittest
 from colour import Color
 from predds_tracker.templatetags import map_colour
+from predds_tracker.models import Character
 
 
 class MapColourTests(unittest.TestCase):
@@ -32,4 +33,26 @@ class MapColourTests(unittest.TestCase):
     def testReturnsThousandKillsColorWhenValueIsOneThousand(self):
         self.assertEqual(
             Color('#DC4D37'), map_colour.npc_kill_colour(1000)
+        )
+
+class CharacterTests(unittest.TestCase):
+    def setUp(self):
+        self.ccp_cart = Character(id=92025524)
+        self.placeholder = Character(id=1206632531, alliance_id=5)
+        self.ccp_cart.update_data()
+        self.placeholder.update_data()
+
+    def testCharacterCorporationIDIsCorrect(self):
+        self.assertEqual(
+            self.ccp_cart.corporation_id, 98356193
+        )
+
+    def testCharacterAllianceIDIsCorrect(self):
+        self.assertEqual(
+            self.ccp_cart.alliance_id, 434243723
+        )
+
+    def testCharacterAllianceIDIsNull(self):
+        self.assertEqual(
+            self.placeholder.alliance_id, None
         )
