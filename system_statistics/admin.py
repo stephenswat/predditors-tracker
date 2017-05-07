c9e4b33f287b27c4b513f92b586f6b4b97e0031a
Stephen Swatman <stephenswat@gmail.com>
2017-05-07 02:57:40 +0200

    Add an admin interface for the stats.

--- system_statistics/admin.py
+++ system_statistics/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from system_statistics.models import SystemStatistic
 
-# Register your models here.
+class SystemStatisticAdmin(admin.ModelAdmin):
+    def get_system_name(self, obj):
+        return obj.system.name
+
+    get_system_name.short_description = 'System Name'
+
+    fieldsets = (
+        (None, {'fields': (
+            'time',
+            'system'
+        )}),
+        ('Statistics', {'fields': (
+            'ship_kills',
+            'pod_kills',
+            'npc_kills',
+            'ship_jumps'
+        )}),
+    )
+
+    list_display = (
+        'time',
+        'get_system_name'
+    )
+
+    ordering = (
+        'time',
+    )
+
+admin.site.register(SystemStatistic, SystemStatisticAdmin)
