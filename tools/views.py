f2609a6d24606bffcb9c2e21c5edb9ccc4d0fa71
Stephen Swatman <stephenswat@gmail.com>
2017-06-16 14:04:43 +0200

    Protect the new tool against outsiders.

--- tools/views.py
+++ tools/views.py
@@ -1,5 +1,8 @@
+from predds_tracker.models import Character
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
 
-# Create your views here.
+@login_required
+@user_passes_test(Character.alliance_valid, login_url='/logout/')
 def warp_bomb(request):
     return render(request, 'tools/warp_time.html')
