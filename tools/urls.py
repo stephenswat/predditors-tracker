a0e9fb82e28d4036e36bbded704f192e4480bdbe
Stephen Swatman <stephenswat@gmail.com>
2017-06-16 14:12:19 +0200

    Add a navbar item for the toolkit.

--- tools/urls.py
+++ tools/urls.py
@@ -2,5 +2,5 @@ from tools import views
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^warp_bomb/', views.warp_bomb),
+    url(r'^warp_bomb/', views.warp_bomb, name='warp_bomb'),
 ]
