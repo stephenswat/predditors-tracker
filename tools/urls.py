7ece3e8a150ff1fd240a0c2b12d4109b3b882ee9
Stephen Swatman <stephenswat@gmail.com>
2017-06-23 01:43:55 +0200

    Add a bomb damage tool.

--- tools/urls.py
+++ tools/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^warp_bomb/', views.warp_bomb, name='warp_bomb'),
+    url(r'^bomb_damage/', views.bomb_damage, name='bomb_damage'),
 ]
